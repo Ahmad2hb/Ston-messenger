5cf8b2d8c12962c3da275b098413d4ac141cd83d
Ahmad <ahmad90lattakia@gmail.com>
2026-09-18 17:13:13 +0300

    Add usernames

--- server.js
+++ server.js
@@ -6,15 +6,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get("/", (req, res) => {
-  res.send("Messenger Server is running");
-});
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
   console.log("جهاز اتصل:", socket.id);
 
-  socket.on("message", (msg) => {
-    io.emit("message", msg);
+  socket.on("chat message", (data) => {
+    io.emit("chat message", data);
   });
 
   socket.on("disconnect", () => {
